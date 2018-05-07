58eda224a90ba592425f56ec0bdd02ebb77f1ba4
Kostas Douzis <kdmortal@gmail.com>
2018-05-07 21:42:43 +0300

    Initial commit with the maven stracture

---
+++ src/main/java/com/codehub/acme/eshop/AcmeEshopApplication.java
@@ -0,0 +1,12 @@
+package com.codehub.acme.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeEshopApplication.class, args);
+	}
+}
