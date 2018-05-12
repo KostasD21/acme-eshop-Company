041fd8daf51d30d78cc1d8458c70d1b4e685518a
elisavetkav <elisavetkav@gmail.com>
2018-05-12 12:28:06 +0300

    Create User and Mail Services

---
+++ src/main/java/com/codehub/acme/eshop/service/MailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.codehub.acme.eshop.service;
+
+public class MailServiceImpl implements MailService {
+
+    /**
+     *  {inheritDoc}
+     */
+    @Override
+    public void sendMail(String email) {
+
+    }
+}
---
+++ src/main/java/com/codehub/acme/eshop/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.codehub.acme.eshop.service;
+
+
+import com.codehub.acme.eshop.domain.User;
+
+public class UserServiceImpl implements UserService {
+
+    /**
+     *  {inheritDoc}
+     */
+    @Override
+    public void registerUser() {
+
+    }
+
+    /**
+     *  {inheritDoc}
+     */
+    @Override
+    public void authenticateUser(String password) {
+
+    }
+
+    /**
+     *  {inheritDoc}
+     */
+    @Override
+    public User getUserInfo(Long userId) {
+        return null;
+    }
+
+    /**
+     *  {inheritDoc}
+     */
+    @Override
+    public void updateUserInfo(Long userId) {
+
+    }
+}
