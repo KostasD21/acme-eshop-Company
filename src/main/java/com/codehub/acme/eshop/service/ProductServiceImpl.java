c525140c6220b453060b3c45832898ba2abb8e8b
AxilleasMoukoulis <axilleasmoukoulis@gmail.com>
2018-05-13 16:38:51 +0300

    The ProductService and ProductServiceImpl pushed with the necessary javadocs

--- src/main/java/com/codehub/acme/eshop/service/ProductServiceImpl.java
+++ src/main/java/com/codehub/acme/eshop/service/ProductServiceImpl.java
@@ -6,31 +6,49 @@ import com.codehub.acme.eshop.domain.Product;
 public class ProductServiceImpl implements ProductService  {
 
 
+    /**
+     *  {inheritDoc}
+     */
     @Override
     public void addProduct(String title, String shortDescription, String longDescription, String productCode, Long quantity, Long stock) {
-        
+
     }
 
+    /**
+     *  {inheritDoc}
+     */
     @Override
     public void removeProduct(Long id) {
 
     }
 
+    /**
+     *  {inheritDoc}
+     */
     @Override
     public void updateProductDetails(String title, String shortDescription, String longDescription, String productCode, Long quantity, Long stock) {
 
     }
 
+    /**
+     *  {inheritDoc}
+     */
     @Override
     public Product findProductByCategoryId(String name) {
         return null;
     }
 
+    /**
+     *  {inheritDoc}
+     */
     @Override
     public Product findProductById(Long id) {
         return null;
     }
 
+    /**
+     *  {inheritDoc}
+     */
     @Override
     public Product findProductByName(String name) {
         return null;
