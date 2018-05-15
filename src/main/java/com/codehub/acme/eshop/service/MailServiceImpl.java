d751bd903fa3c7029fd23855607644dc506b2fdb
elisavetkav <elisavetkav@gmail.com>
2018-05-15 23:41:10 +0300

    Javadoc added

--- src/main/java/com/codehub/acme/eshop/service/MailServiceImpl.java
+++ src/main/java/com/codehub/acme/eshop/service/MailServiceImpl.java
@@ -2,6 +2,11 @@ package com.codehub.acme.eshop.service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * This service contains the implementation of methods regarding the {@link com.codehub.acme.eshop.domain.User} functionality
+ */
+
+
 @Service
 public class MailServiceImpl implements MailService {
 
