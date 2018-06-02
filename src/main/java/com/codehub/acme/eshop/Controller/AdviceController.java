3c20d340b243505f802bda4f6e9dbe918001cca7
elisavetkav <elisavetkav@gmail.com>
2018-06-02 15:02:56 +0300

    Merge branch 'TestingRepositories' into CreateUserLoggin  # Conflicts: #	src/main/java/com/codehub/acme/eshop/domain/Category.java #	src/main/java/com/codehub/acme/eshop/domain/Product.java #	src/main/java/com/codehub/acme/eshop/domain/ShoppingBasket.java #	src/main/java/com/codehub/acme/eshop/domain/User.java #	src/main/java/com/codehub/acme/eshop/repository/UserRepository.java

--- src/main/java/com/codehub/acme/eshop/Controller/AdviceController.java
+++ src/main/java/com/codehub/acme/eshop/Controller/AdviceController.java
@@ -3,6 +3,7 @@ package com.codehub.acme.eshop.Controller;
 
 import com.codehub.acme.eshop.exception.NotFoundException;
 import com.codehub.acme.eshop.exception.ProductOutOfStockException;
+import com.codehub.acme.eshop.exception.ShoppingBasketException;
 import com.codehub.acme.eshop.transformation.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,20 @@ import javax.servlet.http.HttpServletRequest;
 public class AdviceController {
 
     /**
+     * This method handles the {@link ShoppingBasketException}
+     *
+     * @param ex {@link ShoppingBasketException}
+     * @param request the {@link HttpServletRequest}
+     * @return the {@link ErrorMessage}
+     */
+    @ExceptionHandler({ShoppingBasketException.class})
+    public ResponseEntity<ErrorMessage> handleErrors(ShoppingBasketException ex, HttpServletRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorMessage(ex.getMessage()));
+    }
+
+    /**
      * This method handles the {@link NotFoundException}
      *
      * @param ex {@link NotFoundException}
@@ -30,6 +45,51 @@ public class AdviceController {
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorMessage(ex.getMessage()));
     }
+
+//    /**
+//     * This method handles the {@link PasswordInvalidException}
+//     *
+//     * @param ex {@link TokenInvalidException}
+//     * @param request the {@link HttpServletRequest}
+//     * @return the {@link ErrorMessage}
+//     */
+//    @ExceptionHandler({TokenInvalidException.class})
+//    public ResponseEntity<ErrorMessage> handleErrors(TokenInvalidException ex, HttpServletRequest request) {
+//        return ResponseEntity
+//                .status(HttpStatus.UNAUTHORIZED)
+//                .body(new ErrorMessage(ex.getMessage()));
+//    }
+//
+
+
+//    /**
+//     * This method handles the {@link PasswordInvalidException}
+//     *
+//     * @param ex {@link PasswordInvalidException}
+//     * @param request the {@link HttpServletRequest}
+//     * @return the {@link ErrorMessage}
+//     */
+//    @ExceptionHandler({PasswordInvalidException.class})
+//    public ResponseEntity<ErrorMessage> handleInvalidErrors(PasswordInvalidException ex, HttpServletRequest request) {
+//        return ResponseEntity
+//                .status(HttpStatus.BAD_REQUEST)
+//                .body(new ErrorMessage(ex.getMessage()));
+//    }
+
+//    /**
+//     * This method handles the {@link UsernameInvalidException}
+//     *
+//     * @param ex {@link UsernameInvalidException}
+//     * @param request the {@link HttpServletRequest}
+//     * @return the {@link ErrorMessage}
+//     */
+//    @ExceptionHandler({UsernameInvalidException.class})
+//    public ResponseEntity<ErrorMessage> handleInvalidErrors(UsernameInvalidException ex, HttpServletRequest request) {
+//        return ResponseEntity
+//                .status(HttpStatus.BAD_REQUEST)
+//                .body(new ErrorMessage(ex.getMessage()));
+//    }
+
 
     /**
      * This method handles the {@link ProductOutOfStockException}
